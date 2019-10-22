895e09dfb03dfb184e49bd30e643c8a7b8c20308
Eduardo Henrique <eduardo18delta@gmail.com>
2019-10-22 19:46:28 -0300

    Novas rotas do perfil

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,8 @@ Route::get('/' , 'Site\siteController@index')->name('home');
 # Rotas Administrativas
 Route::group(['middleware' => ['auth'], 'namespace' => 'Painel' ] , function(){
 	Route::get('/painel', 'painelController@index')->name('painel.home');
+	Route::get('/painel/perfil', 'painelController@chamaPerfil')->name('painel.perfil');
+	Route::get('/painel/senha', 'painelController@chamaSenha')->name('painel.senha');
 });
 
 # Rotas Autenticadas
