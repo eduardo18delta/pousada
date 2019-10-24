efe981968712eb194b3e345fff3ff84aeab66f9c
Eduardo Henrique <eduardo18delta@gmail.com>
2019-10-24 11:06:58 -0300

    Organizando rotas

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,25 @@
 <?php
 
-# Rotas Públicas
-Route::get('/' , 'Site\siteController@index')->name('home');
+# Rotas da home
+Route::get('/' , 'Site\SiteController@chamaHome')->name('home');
+Route::get('/' , 'Site\SiteController@listaTextos');	
+# Nova rota de Logout
+Route::get('/logout', 'LoginController@logout');
+# Rotas Autenticadas
+Auth::routes();
 
 # Rotas Administrativas
 Route::group(['middleware' => ['auth'], 'namespace' => 'Painel' ] , function(){
-	Route::get('/painel', 'painelController@index')->name('painel.home');
-	Route::get('/painel/perfil', 'painelController@chamaPerfil')->name('painel.perfil');
-	Route::get('/painel/senha', 'painelController@chamaSenha')->name('painel.senha');	
+	Route::get('/painel', 'PainelController@index')->name('painel.home');
+	Route::get('/painel/perfil', 'PainelController@chamaPerfil')->name('painel.perfil');
+	Route::get('/painel/senha',  'PainelController@chamaSenha')->name('painel.senha');	
+	Route::post('/atualizar-perfil' , 'UserController@updatePerfil')->name('perfil.update');
+	Route::post('/atualizar-senha'  , 'UserController@updateSenha')->name('senha.update');
 });
 
-# Nova rota de Logout
-Route::get('/logout', 'LoginController@logout');	
-# Rotas de atualização do perfil
-Route::post('/atualizar-perfil' , 'Painel\userController@updatePerfil')->name('perfil.update')->middleware('auth');
-Route::post('/atualizar-senha'  , 'Painel\userController@updateSenha')->name('senha.update')->middleware('auth');
 
-# Rotas Autenticadas
-Auth::routes();
+
+	
+
+
 
