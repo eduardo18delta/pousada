fc2d0c66e313798fa4e837326cae6c06e3ea8221
Eduardo Henrique <eduardo18delta@gmail.com>
2019-10-24 13:16:40 -0300

    Update das rotas

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,31 @@ Route::group(['middleware' => ['auth'], 'namespace' => 'Painel' ] , function(){
 	Route::get('/painel/senha',  'PainelController@chamaSenha')->name('painel.senha');	
 	Route::post('/atualizar-perfil' , 'UserController@updatePerfil')->name('perfil.update');
 	Route::post('/atualizar-senha'  , 'UserController@updateSenha')->name('senha.update');
+	
+	# Crud dos Textos da Home
+	Route::get('painel/textoshome', 'TextoshomeController@index');
+	Route::get('painel/textoshome/{id}/editar', 'TextoshomeController@edit');
+
+	
+
+
+
 });
+
+
+
+
+
+// Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
+    
+//     Route::get('projetos/inserir', 'ProjetosController@getInserir');
+//     Route::post('projetos/inserir', 'ProjetosController@postInserir');
+//     Route::get('projetos/editar/{id}', 'ProjetosController@getEditar');
+//     Route::post('projetos/editar/{id}', 'ProjetosController@postEditar');
+//     Route::post('projetos/deletar/{id}', 'ProjetosController@postDeletar');
+
+
+
 
 
 
