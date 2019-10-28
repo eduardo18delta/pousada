4a29d54fb5a23d97e9a0c34badff2cfc0f6b1f9e
Eduardo Henrique <eduardo18delta@gmail.com>
2019-10-28 12:09:28 -0300

    Protegendo campos

--- app/Models/textohome.php
+++ app/Models/textohome.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class textohome extends Model
 {
     protected $fillable = ['titulo' , 'descricao'];
+    protected $guarded = ['id', 'created_at', 'update_at'];
     protected $table = 'textoshome';
-
 }
